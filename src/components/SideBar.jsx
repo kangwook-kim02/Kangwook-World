a21a515665ae30e48b90bdb223a9fe7f7849d066
kangwook-kim02 <cocacolra741@naver.com>
2026-01-09 03:43:09 +0900

    svg 인식을 위한 vite-plugin-svgr 설치 및 기술 Icon UI 구현

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,4 +1,9 @@
 import "./SideBar.css";
+import profileImage from "./images/profileImage.png";
+import CppIcon from "./icons/cplusplus.svg?react";
+import PythonIcon from "./icons/python.svg?react";
+import JavaScriptIcon from "./icons/javascript.svg?react";
+import ReactIcon from "./icons/react.svg?react";
 
 const SideBar = () => {
     return <div className="SideBar">
@@ -6,9 +11,31 @@ const SideBar = () => {
         <div className="ProfileOutline">
             <div className="ProfileInline">
                 <div className="ImageBox">
-                    <img src="" alt="" />
+                    <img src={profileImage} alt="" />
                 </div>
                 <div className="Divider"></div>
+                <div className="Introduce">
+                    안녕하세요. 개발자 김강욱입니다.
+                    깊이 있는 학습을 기반으로 성장하는 개발자가 되겠습니다.
+                </div>
+                <div className="Divider"></div>
+
+                <h4 className="SkillTitle">기술(Skill)</h4>
+                <div className="SkillList">
+                    <div className="Skill">
+                        <CppIcon className="CplusplusIcon" /> <span className="CplusplusText">C++</span>
+                    </div>
+                    <div className="Skill">
+                        <PythonIcon className="PythonIcon" /> <span className="pythonText">Python</span>
+                    </div>
+                    <div className="Skill">
+                        <JavaScriptIcon className="JavaScriptIcon" /> <span className="JavaScriptText">JavaScript</span>
+                    </div>
+                    <div className="Skill">
+                        <ReactIcon className="ReactIcon" /> <span className="ReactText">React</span>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div >
