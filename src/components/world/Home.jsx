20ef7b76885e312f1582a9dd6a7111ca24394f10
kangwook-kim02 <cocacolra741@naver.com>
2026-01-11 07:44:51 +0900

    Home github, boj 카드 UI 구현

--- src/components/world/Home.jsx
+++ src/components/world/Home.jsx
@@ -1,6 +1,7 @@
 import "./Home.css"
 import { useEffect, useState } from "react";
-
+import githubImage from "../images/github.png";
+import bojImage from "../images/boj.png";
 const Home = ({ onClickButton }) => {
 
 
@@ -51,18 +52,40 @@ const Home = ({ onClickButton }) => {
         <div className="HomeOutline">
             <div id="player">
             </div>
-            <div className="Music">
-                <div className="MusicName">
-                    <span className={`cd ${playing === "⏸" ? "spinning" : ""}`}>💿</span>
-                    {playing === "⏸" ? (
-                        <marquee scrollamount="3">프리스타일 - Y (Please Tell Me Why)</marquee>
-                    ) : (
-                        <div>프리스타일 - Y (Please Tell Me Why)</div>
-                    )}
+
+            <div className="top">
+                <div className="Music">
+                    <div className="MusicName">
+                        <span className={`cd ${playing === "⏸" ? "spinning" : ""}`}>💿</span>
+                        {playing === "⏸" ? (
+                            <marquee scrollamount="3">프리스타일 - Y (Please Tell Me Why)</marquee>
+                        ) : (
+                            <div>프리스타일 - Y (Please Tell Me Why)</div>
+                        )}
+                    </div>
+                    <button onClick={playMusic} value={playing}>{playing}</button>
+                    <button onClick={restartMusic}>■</button>
                 </div>
-                <button onClick={playMusic} value={playing}>{playing}</button>
-                <button onClick={restartMusic}>■</button>
+
+                <div className="github">
+                    <img src={githubImage} />
+                    <div className="info">
+                        <div className="name">GitHub</div>
+                        <div className="handle"><a href="https://github.com/kangwook-kim02" target='_blank' rel="noopener noreferrer">@kangwook-kim02</a></div>
+                    </div>
+                </div>
+
+                <div className="boj">
+                    <img src={bojImage} />
+                    <div className="info">
+                        <div className="name">BaekJoon</div>
+                        <div className="handle"><a href="https://solved.ac/profile/ices17" target='_blank' rel="noopener noreferrer">@ices17</a></div>
+                    </div>
+                    <div className="badge">G4</div>
+                </div>
+
             </div>
+
             <div className="HomeMain">
 
             </div>
