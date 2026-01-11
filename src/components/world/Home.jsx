70775182c1b64de7f0b28399dbe0fd4b97f3bade
kangwook-kim02 <cocacolra741@naver.com>
2026-01-12 02:45:08 +0900

    HomeMain 디자인 개선

--- src/components/world/Home.jsx
+++ src/components/world/Home.jsx
@@ -92,15 +92,17 @@ const Home = ({ onClickButton }) => {
             <div className="HomeMain">
                 <div className="Inline">
                     <div className="Top">
-                        <h3>주요 역량</h3>
+                        <h3>핵심 역량</h3>
                     </div>
                     <div className="bottom">
                         <div className="computerScience">
                             <img className="csImage" src={csImage} alt="csImage" />
-                            <p>컴퓨터공학과 전공을 하면서 자료구조, OS, DB, 컴퓨터네트워크, 알고리즘 등 깊이 있는 CS 지식을 학습하였습니다.</p>
+                            <p style={{ marginBottom: 5, fontSize: "18px", fontWeight: "600", color: "black" }}>깊이 있는 전공지식</p>
+                            <p>컴퓨터공학과 전공을 하면서 자료구조, OS, DB, 컴퓨터 네트워크, 알고리즘 등 깊이 있는 CS 지식을 학습하였습니다.</p>
                         </div>
                         <div className="fullstack">
                             <img className="fsImage" src={fullstackImage} alt="fullstackImage" />
+                            <p style={{ marginBottom: 5, fontSize: "18px", fontWeight: "600", color: "black" }}>풀스택 개발</p>
                             <p>Spring을 이용한 백엔드 개발경험이 있으며, 현재는 프론트엔드를 집중해서 학습하고 있습니다.</p>
                         </div>
                     </div>
