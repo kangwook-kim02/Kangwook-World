72a48cb222af966c57fce2226c2690db63d3b2f2
KangwookKim <nghso7572@gmail.com>
2026-01-25 23:19:05 +0900

    PacRep 링크 연결 에러 해결

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,30 +1,25 @@
-import "./Main.css"
+import "./Main.css";
 import Home from "./world/Home";
 import Profile from "./world/Profile";
 import Project from "./world/Project";
 import { useState } from "react";
 
 const Main = () => {
+  // pageState
+  // 0: Home, 1: Profile, 2: Project
+  const [pageState, setPageState] = useState(0);
 
+  const onClickButton = (e) => {
+    setPageState(Number(e.target.value));
+  };
 
-    // pageState
-    // 0: Home, 1: Profile, 2: Project 
-    const [pageState, setPageState] = useState(0);
-
-    const onClickButton = (e) => {
-        setPageState(Number(e.target.value));
-    }
-
-    if (pageState === 0) {
-        return <Home onClickButton={onClickButton} pageState={pageState} />;
-    }
-    else if (pageState === 1) {
-        return <Profile onClickButton={onClickButton} pageState={pageState} />;
-    }
-    else if (pageState === 2) {
-        return <Project onClickButton={onClickButton} pageState={pageState} />;
-    }
-
-}
+  if (pageState === 0) {
+    return <Home onClickButton={onClickButton} pageState={pageState} />;
+  } else if (pageState === 1) {
+    return <Profile onClickButton={onClickButton} pageState={pageState} />;
+  } else if (pageState === 2) {
+    return <Project onClickButton={onClickButton} pageState={pageState} />;
+  }
+};
 
 export default Main;
