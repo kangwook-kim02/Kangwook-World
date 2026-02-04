8299ec62fca66f23011cb749813be072dd6711a3
kangwook-kim02 <cocacolra741@naver.com>
2026-02-04 22:40:20 +0900

    프로필 JSON data 추가 및 프로필 내용 리팩토링

---
+++ src/components/data/profileData.js
@@ -0,0 +1,15 @@
+export const profileData = {
+  name: "김강욱 (2002.08.20)",
+  education: "2022.2~2027.2 인하대학교(학사)/컴퓨터공학",
+  interests: "웹개발, 네트워크, 데이터마이닝, AI",
+  history: {
+    name: "2025.6 ~ 2025.12 학부연구생, DNSLab, 인하대학교.",
+    detail:
+      "ICT융합 공공 서비스·인프라의 암호화 사이버위협에 대한 네트워크 행위기반 보안관제 기술 개발(RS-2023-00235509)",
+  },
+  award: {
+    paper: `K. Kim, and H. Roh, "Per- Flow Split Evaluation for Packet Representation Learning: Data Snooping and Payload Dependency in Encrypted Traffic Classification", Korea Software Congress 2025 (KSC 2025) , Dec.2025. [학부생부문 우수상]`,
+    swcarbon:
+      "2025.12.22 인하대학교 탄소중립아카데미 최우수상 - 팀 Inha DeepMind",
+  },
+};
