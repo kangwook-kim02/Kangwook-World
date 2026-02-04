9644007bd16fc1276d10aff9c01e9a790d51dd6c
kangwook-kim02 <cocacolra741@naver.com>
2026-02-04 22:50:08 +0900

    논문 제목 수정

--- src/components/data/profileData.js
+++ src/components/data/profileData.js
@@ -8,7 +8,7 @@ export const profileData = {
       "ICT융합 공공 서비스·인프라의 암호화 사이버위협에 대한 네트워크 행위기반 보안관제 기술 개발(RS-2023-00235509)",
   },
   award: {
-    paper: `K. Kim, and H. Roh, "Per- Flow Split Evaluation for Packet Representation Learning: Data Snooping and Payload Dependency in Encrypted Traffic Classification", Korea Software Congress 2025 (KSC 2025) , Dec.2025. [학부생부문 우수상]`,
+    paper: `K. Kim, and H. Roh, "Per-Flow Split Evaluation for Packet Representation Learning: Data Snooping and Payload Dependency in Encrypted Traffic Classification", Korea Software Congress 2025 (KSC 2025) , Dec.2025. [학부생부문 우수상]`,
     swcarbon:
       "2025.12.22 인하대학교 탄소중립아카데미 최우수상 - 팀 Inha DeepMind",
   },
